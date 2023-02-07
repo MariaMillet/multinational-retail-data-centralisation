91dfd597d7f2674c90fc46718a20a3ac5409827c
mmillet88 <m.kosyuchenko@gmail.com>
2023-02-07 17:53:37 +0000

    Classes created.

---
+++ data_extraction.py
@@ -0,0 +1,4 @@
+class DataExtractor:
+    pass
+    def read_rds_table(self, connector_instance, table_name):
+        
---
+++ database_utils.py
@@ -0,0 +1,46 @@
+import yaml
+import sqlalchemy as db
+from sqlalchemy import create_engine, engine_from_config, URL, inspect
+
+
+class DatabaseConnector:
+    def __init__(self):
+        pass
+
+    def read_db_creds(self):
+        with open('db_creds.yaml', 'r') as file:
+            config = yaml.safe_load(file)
+        return config
+
+    def init_db_engine(self):
+        config = self.read_db_creds()
+        url_object = URL.create(
+            "postgresql+psycopg2",
+            username=config['RDS_USER'],
+            password=config['RDS_PASSWORD'],  # plain (unescaped) text
+            host=config['RDS_HOST'],
+            database=config['RDS_DATABASE'],
+            port=config['RDS_PORT']
+        )
+        engine = create_engine(url_object )
+        return engine
+
+    def list_db_tables(self):
+        engine = self.init_db_engine()
+        inspector = inspect(engine)
+        return inspector.get_table_names()
+    
+    def upload_to_db(self, df, table_name, engine):
+        df.to_sql(table_name, engine, if_exists='replace')
+    
+
+
+
+    
+
+if __name__ == "__main__":
+    db = DatabaseConnector()
+    db_names = db.list_db_tables()
+    print(db_names)
+
+
